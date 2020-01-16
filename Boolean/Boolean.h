a2397ff2a5b30d07ead013e6773b7215bc872799
Serhii <serhiisennyk@gmail.com>
2020-01-16 14:32:32 +0200

    Boolean_1

---
+++ Boolean/Boolean.h
@@ -0,0 +1,79 @@
+bool Boolean1(int a);
+
+bool Boolean2(int a);
+
+bool Boolean3(int a);
+
+bool Boolean4(int a, int b);
+
+bool Boolean5(int a, int b);
+
+bool Boolean6(int a, int b, int c);
+
+bool Boolean7(int a, int b, int c);
+
+bool Boolean8(int a, int b);
+
+bool Boolean9(int a, int b);
+
+bool Boolean10(int a, int b);
+
+bool Boolean11(int a, int b);
+
+bool Boolean12(int a, int b, int c);
+
+bool Boolean13(int a, int b, int c);
+
+bool Boolean14(int a, int b, int c);
+
+bool Boolean15(int a, int b, int c);
+
+bool Boolean16(int a);
+
+bool Boolean17(int a);
+
+bool Boolean18(int a, int b, int c);
+
+bool Boolean19(int a, int b, int c);
+
+bool Boolean20(int a);
+
+bool Boolean21(int a);
+
+bool Boolean22(int a);
+
+bool Boolean23(int a);
+
+bool Boolean24(int a, int b, int c);
+
+bool Boolean25(int x, int y);
+
+bool Boolean26(int x, int y);
+
+bool Boolean27(int x, int y);
+
+bool Boolean28(int x, int y);
+
+bool Boolean29(int x, int y, int x1, int y1, int x2, int y2);
+
+bool Boolean30(int a, int b, int c);
+
+bool Boolean31(int a, int b, int c);
+
+bool Boolean32(int a, int b, int c);
+
+bool Boolean33(int a, int b, int c);
+
+bool Boolean34(int x, int y);
+
+bool Boolean35(int x1, int y1, int x2, int y2);
+
+bool Boolean36(int x1, int y1, int x2, int y2);
+
+bool Boolean37(int x1, int y1, int x2, int y2);
+
+bool Boolean38(int x1, int y1, int x2, int y2);
+
+bool Boolean39(int x1, int y1, int x2, int y2);
+
+bool Boolean40(int x1, int y1, int x2, int y2);
---
+++ Boolean/Main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Boolean.h"
+
+void main() {
+	Boolean39(3, 3, 5, 6);
+	system("pause");
+}
